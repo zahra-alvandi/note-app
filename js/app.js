c04836df02311eb522fde7152495a1580ccc978b
Zahra Alvandi <zalvandi39@gmail.com>
2025-08-02 11:24:53 +0330

    part-1

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const creatBtn = document.querySelector(".creat-button");
+const modalScreen = document.querySelector(".modal-screen");
+const modal = document.querySelector(".modal");
+
+creatBtn.addEventListener("click", function() {
+    modalScreen.classList.remove("hidden")
+})
